7516e13b6c91ca9cb02b30bbbf982579c89b943e
dojinyou <djyou128@gmail.com>
2023-02-12 22:41:41 +0900

    토비의 스프링부트 - API Test

---
+++ example/src/test/kotlin/com/dojinyou/inflearn/tobyspringboot/example/HelloApiTest.kt
@@ -0,0 +1,23 @@
+package com.dojinyou.inflearn.tobyspringboot.example
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+
+class HelloApiTest {
+
+	@Test
+	fun contextLoads() {
+		val rest = TestRestTemplate()
+
+		val res = rest.getForEntity("http://localhost:8080/hello?name={name}", String::class.java, "Spring")
+
+		assertThat(res.statusCode).isEqualTo(HttpStatus.OK)
+		assertThat(res.headers[HttpHeaders.CONTENT_TYPE]!!.first()).startsWith(MediaType.TEXT_PLAIN_VALUE)
+		assertThat(res.body).startsWith("Hello Spring")
+	}
+
+}
