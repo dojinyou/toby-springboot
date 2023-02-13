2d8c22727ca5fb9809713553124c7ed2bc4098bb
dojinyou <djyou128@gmail.com>
2023-02-13 23:22:52 +0900

    토비의 스프링부트 - annotation 이름 수정 및 boot run 함수 위치 이동

--- example/src/main/kotlin/com/dojinyou/inflearn/tobyspringboot/example/ExampleApplication.kt
+++ example/src/main/kotlin/com/dojinyou/inflearn/tobyspringboot/example/ExampleApplication.kt
@@ -1,6 +1,14 @@
 package com.dojinyou.inflearn.tobyspringboot.example
 
-@MySpringBootAnnotation
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
+
+inline fun<reified T: Any> runApplication(vararg args: String) {
+    val applicationContext: AnnotationConfigWebApplicationContext = MyAnnotationConfigWebApplicationContext()
+    applicationContext.register(T::class.java)
+    applicationContext.refresh()
+}
+
+@MySpringBootApplication
 class ExampleApplication
 
 fun main(args: Array<String>) {
