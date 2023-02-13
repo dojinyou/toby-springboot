0ae523fd763dc1681e9945ff30b9e3f167213851
dojinyou <djyou128@gmail.com>
2023-02-13 23:10:39 +0900

    토비의 스프링부트 - 합성 Annotation으로 변환하기

--- example/src/main/kotlin/com/dojinyou/inflearn/tobyspringboot/example/ExampleApplication.kt
+++ example/src/main/kotlin/com/dojinyou/inflearn/tobyspringboot/example/ExampleApplication.kt
@@ -1,27 +1,8 @@
 package com.dojinyou.inflearn.tobyspringboot.example
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.DispatcherServlet
-
-@Configuration
-@ComponentScan
-class ExampleApplication {
-    @Bean
-    fun servletWebServerFactory(): ServletWebServerFactory {
-        return TomcatServletWebServerFactory()
-    }
-
-    @Bean
-    fun dispatcherServlet(): DispatcherServlet {
-        return DispatcherServlet()
-    }
-}
+@MySpringBootAnnotation
+class ExampleApplication
 
 fun main(args: Array<String>) {
     runApplication<ExampleApplication>(*args)
 }
-
---
+++ example/src/main/kotlin/com/dojinyou/inflearn/tobyspringboot/example/MySpringBootAnnotation.kt
@@ -0,0 +1,10 @@
+package com.dojinyou.inflearn.tobyspringboot.example
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+@Configuration
+@ComponentScan
+annotation class MySpringBootAnnotation
